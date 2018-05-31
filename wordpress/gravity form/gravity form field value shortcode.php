502c1d00c797fcafd64b2cac7330ba3f84200a30
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-05-31 14:33:03 +0700

    Get custom field in category

--- wordpress/gravity form/gravity form field value shortcode.php
+++ wordpress/gravity form/gravity form field value shortcode.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Gravity form shortcode with custom value
+ * https://docs.gravityforms.com/allow-field-to-be-populated-dynamically/
  */
 $shortcode = '[gravityform id=2 title=false description=false ajax=true field_values="job_title='.$job->title.'"]';
 echo do_shortcode($shortcode);
