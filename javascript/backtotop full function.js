463ae8be6165f9ec6d0b09348a813d19d390fafc
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-06-07 14:51:20 +0700

    Back to top completed

---
+++ javascript/backtotop full function.js
@@ -0,0 +1,71 @@
+/**
+ * Append back to top button to footer
+ */
+$("footer").append('<span class="backtotop"></span>');
+
+/**
+ * Back to top button
+ */
+$('.backtotop').on('click', function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: 0 /* element.offset().top */
+    }, 700);
+});
+
+/**
+ * Detect scroll direction
+ */
+var position = $(window).scrollTop();
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > position) {
+        $('.backtotop').removeClass('showing');
+    } else {
+        $('.backtotop').addClass('showing');
+    }
+    position = scroll;
+});
+
+/**
+ * Detect touch to top
+ */
+function detectScrollToTop(scrollTrigger = 100){
+    $(window).on('load scroll', function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > scrollTrigger) {
+
+        } else {
+            $('.backtotop').removeClass('showing');
+        }
+    });
+}
+detectScrollToTop();
+
+/**
+ * Detect touch to bottom
+ */
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        $('.backtotop').addClass('showing');
+    }
+};
+
+/**
+ * CSS
+ */
+/*
+.backtotop {
+    position:fixed; right:10px; bottom:10px; cursor:pointer;
+    width:40px; height:40px; background:rgba(253, 152, 32, 1);
+    border:2px solid #fff;
+    -webkit-transition:all .3s ease;-o-transition:all .3s ease;-moz-transition:all .3s ease;transition:all .3s ease;
+    opacity:0; visibility:hidden;
+}
+.backtotop:before {
+    content:"\f106"; font-family:"fontawesome"; font-size:25px; color:#fff;
+    position:absolute; top:-4px; left:11px;
+}
+.backtotop.showing {opacity:.5; visibility:visible;}
+.backtotop.showing:hover {opacity:1;}
+*/
