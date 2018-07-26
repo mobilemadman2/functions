0a4ae9d977e44c16e181e2c4fcf5a0a570301457
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-07-26 23:01:27 +0700

    Check if element is visible after scrolling

--- javascript/detect when scroll to div.js
+++ javascript/detect when scroll to div.js
@@ -1,15 +1,34 @@
-/*** detect when element on the view ***/
-    function onTheView(element){
-        var ele = $(element);
-        $(window).scroll(function() {
-            var hT = ele.offset().top,
-                hH = ele.outerHeight(),
-                wH = $(window).height(),
-                wS = $(this).scrollTop();
-            if (wS > (hT+hH-wH)){
-                console.log('H1 on the view!');
-                $('#blog-loadmore').trigger('click');
-            }
-        });
+/**
+ * Check if element is visible after scrolling
+ * https://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+ * @constructor
+ */
+function Utils() {
+}
+
+Utils.prototype = {
+    constructor: Utils,
+    isElementInView: function (element, fullyInView) {
+        var pageTop = $(window).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var elementTop = $(element).offset().top;
+        var elementBottom = elementTop + $(element).height();
+
+        if (fullyInView === true) {
+            return ((pageTop < elementTop) && (pageBottom > elementBottom));
+        } else {
+            return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+        }
     }
-    onTheView('#blog-loadmore');
+};
+
+var Utils = new Utils();
+$(window).on("load scroll", function () {
+    var isElementInView = Utils.isElementInView($('section.drl-main-stats'), false);
+
+    if (isElementInView) {
+        console.log('in view');
+    } else {
+        console.log('out of view');
+    }
+});
