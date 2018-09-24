ae32ddcddb217af913bb0f368dc52875e649cac5
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-09-24 17:38:37 +0700

    Parallax using background image

--- javascript/parallax background image.js
+++ javascript/parallax background image.js
@@ -1,14 +1,18 @@
 /**
  * Parallax using background image
  */
-if ($(window).width() > 1025) {
-    var offset = 0;
-    var wScroll = 0;
-    $(window).on("scroll", function () {
-        var scroll_window = $(window).scrollTop();
-        if (scroll_window > offset) {
-            wScroll = (scroll_window - offset) * 0.65;
-            $(element).css('background-position', '50% ' + (-150 + wScroll) + 'px');
+function backgroundParallax(el){
+    $(window).on('load resize', function () {
+        if ($(window).width() > 1025) {
+            var offset = 0;
+            var wScroll = 0;
+            $(window).on("scroll", function () {
+                var scroll_window = $(window).scrollTop();
+                if (scroll_window > offset) {
+                    wScroll = (scroll_window - offset) * 0.65;
+                    $(el).css('background-position', '50% ' + (-150 + wScroll) + 'px');
+                }
+            });
         }
     });
 }
