eba1092657e79e51401253492f9de279bec3541f
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-09-26 14:21:15 +0700

    Get language switcher WPML

--- wordpress/wpml/wpml get language switcher.php
+++ wordpress/wpml/wpml get language switcher.php
@@ -4,10 +4,31 @@
  * https://wpml.org/documentation/getting-started-guide/language-setup/custom-language-switcher/
  */
 function get_language_switcher() {
-	// List of languages
+	// Get raw arrays of languages
 	$languages = icl_get_languages( 'skip_missing=0&orderby=custom&order=asc&link_empty_to=str' );
 
+	// Loop
+	$active_item = '';
+	$list_items  = '<ul class="language-list">';
 	foreach ( $languages as $language ) {
-		echo $language;
+		$native_name      = $language['native_name'];
+		$url              = $language['url'];
+		$active           = $language['active'];
+		//$translated_name  = $language['translated_name'];
+		//$country_flag_url = $language['country_flag_url'];
+
+		if ( $active ) {
+			$active_item = '<span class="active">' . $native_name . '</span>';
+		} else {
+			$list_items .= '<li><a href="' . $url . '">' . $native_name . '</a></li>';
+		}
 	}
+	$list_items .= '</ul>';
+
+	// Build html
+	$lang_switcher = '<div class="language-switcher">';
+	$lang_switcher .= $active_item . $list_items;
+	$lang_switcher .= '</div>';
+
+	return $lang_switcher;
 }
