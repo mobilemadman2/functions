8801b5c2a87956c0a899e884d0ff695d94205c22
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-10-03 17:57:13 +0700

    add content-related

---
+++ wordpress/content-related.php
@@ -0,0 +1,44 @@
+<?php
+$categories     = get_the_category( get_the_ID() );
+$category_class = '';
+if ( count( $categories ) > 0 ) {
+	foreach ( $categories as $category ) {
+		$category_class .= ' ' . $category->slug;
+	}
+}
+$link = get_the_permalink();
+$img  = wp_get_attachment_image_url( get_post_thumbnail_id(), "post-thumbnail" );
+if ( ! empty( get_field( "news_image_overview" ) ) ) {
+	$img = get_field( "news_image_overview" );
+}
+if ( empty( $img ) ) {
+	$default = get_field( "default_thumbnail", "option" );
+	$img     = $default["sizes"]["post-thumbnail"];
+}
+$title     = get_the_title();
+$category_name = '';
+if(get_post_type(get_the_ID()) != 'post'){
+    $category_name = get_post_type(get_the_ID());
+}else{
+    $category_id = twc_get_primary_category(get_the_ID());
+    $term = get_term_by( 'id', absint( $category_id ), 'category' );
+    if($term){
+        $category_name = $term->name;
+    }else{
+        global $wp_post_types;
+        $obj = $wp_post_types['post'];
+        $category_name = $obj->labels->singular_name;
+    }
+}
+?>
+<div class="item element-item<?= $category_class; ?>" data-year="<?= $year ?>">
+    <div class="item-inner" data-link="<?= $link ?>">
+        <div class="item-thumbnail" style="background-image: url('<?= $img; ?>')">
+            <img src="<?= $img; ?>" alt="<?= $title; ?>"/>
+        </div>
+        <div class="item-info">
+            <div class="item-type"><?= $category_name; ?></div>
+            <h3 class="item-title"><?= $title ?></h3>
+        </div>
+    </div>
+</div>
