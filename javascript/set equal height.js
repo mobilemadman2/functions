094690f3338fecdf9109ea53a70b8cba32663770
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-11-09 13:52:26 +0700

    Update javascript/set equal height.js

--- javascript/set equal height.js
+++ javascript/set equal height.js
@@ -2,18 +2,14 @@
  * Set equal height on load, resize
  * @param $items
  */
-function set_equal_height($items) {
+TWC.setEqualHeight = function (el) {
+    var max_h, $items = $(el);
     $(window).on('load resize', function () {
-        var max_h = 0;
-        // Reset height
+        max_h = 0;
         $items.outerHeight('');
-
-        // Find max height
         $items.each(function () {
             max_h = $(this).outerHeight() > max_h ? $(this).outerHeight() : max_h;
         });
-
-        // Set max height
         $items.outerHeight(max_h);
     });
-}
+};
