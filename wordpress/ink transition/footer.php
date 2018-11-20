6eaf48ad8989ae31fbdf7e00ad1a6bf5fa191e35
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-11-20 10:24:03 +0700

    ink transition

--- wordpress/ink transition/footer.php
+++ wordpress/ink transition/footer.php
@@ -1,13 +1,13 @@
 <!-- Transition Layer -->
-<div class="transition-layer visible">
+<div class="ink-transition visible">
 
 	<!-- Contains ink transition -->
-	<div class="background-layer"></div>
+	<div class="ink-layer"></div>
 
 	<!-- Hide opening effect when page has not loaded yet -->
 	<div class="solid-layer"></div>
 
 	<!-- Trigger buttons -->
-	<button class="modal-open">OPEN</button>
-	<button class="modal-close">CLOSE</button>
+	<button id="ink-open">OPEN</button>
+	<button id="ink-close">CLOSE</button>
 </div>
