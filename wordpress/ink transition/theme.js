251b042db594164da1cb8b16336b8a2532a90364
Bùi Minh Phúc <minhphuc1511one@gmail.com>
2018-11-20 10:24:03 +0700

    ink transition

--- wordpress/ink transition/theme.js
+++ wordpress/ink transition/theme.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function ($) {
                 // Remove loading class after page loaded
                 $('body').removeClass('twc-page-loading');
                 $('body').addClass('twc-page-loaded');
-                $('.modal-close').trigger('click');
+                $('#ink-close').trigger('click');
 
                 // Add loading class when user leave page
                 window.onbeforeunload = on_leaving_page;
@@ -23,8 +23,67 @@ jQuery(document).ready(function ($) {
         });
     };
 
+    TWC.inkTransitionEffect = function () {
+        // cache some jQuery objects
+        var modalTrigger = $('#ink-open'),
+            modalClose = $('#ink-close'),
+            transitionLayer = $('.ink-transition'),
+            transitionBackground = transitionLayer.children();
+
+        var frameProportion = 1.78, //png frame aspect ratio
+            frames = 25, //number of png frames
+            resize = false;
+
+        // set transitionBackground dimentions
+        setLayerDimensions();
+        $(window).on('resize', function () {
+            if (!resize) {
+                resize = true;
+                (!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
+            }
+        });
+
+        // open modal window
+        modalTrigger.on('click', function (event) {
+            event.preventDefault();
+            transitionLayer.addClass('visible opening');
+        });
+
+        // close modal window
+        modalClose.on('click', function (event) {
+            event.preventDefault();
+            transitionLayer.addClass('closing');
+            transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function () {
+                transitionLayer.removeClass('closing opening visible');
+                transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+            });
+        });
+
+        function setLayerDimensions() {
+            var windowWidth = $(window).width(),
+                windowHeight = $(window).height(),
+                layerHeight, layerWidth;
+
+            if (windowWidth / windowHeight > frameProportion) {
+                layerWidth = windowWidth;
+                layerHeight = layerWidth / frameProportion;
+            } else {
+                layerHeight = windowHeight * 1.2;
+                layerWidth = layerHeight * frameProportion;
+            }
+
+            transitionBackground.css({
+                'width': layerWidth * frames + 'px',
+                'height': layerHeight + 'px'
+            });
+
+            resize = false;
+        }
+    };
+
     TWC.loadFunctions = function () {
         TWC.handleLoadingState();
+        TWC.inkTransitionEffect();
     };
 
     TWC.loadFunctions();
@@ -40,7 +99,7 @@ function on_leaving_page() {
     if (!mailtoClicked) {
         jQuery('body').addClass('twc-page-leaving');
         jQuery('body').removeClass('twc-page-loaded');
-        jQuery('.modal-open').trigger('click');
+        jQuery('#ink-open').trigger('click');
     } else {
         mailtoClicked = false;
     }
