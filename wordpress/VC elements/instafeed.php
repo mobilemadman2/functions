9b0e196e9b16673323cc93aec3c9849dc18d5a6f
phucbm <hiphucbui@gmail.com>
2018-11-26 14:27:38 +0700

    Update instafeed.php

--- wordpress/VC elements/instafeed.php
+++ wordpress/VC elements/instafeed.php
@@ -1,26 +1,32 @@
 <?php
 define( 'VC_ICON', THEME_URL . '/assets/images/vc-icon.png' );
-define( 'VC_CATEGORY', __( 'JONAS Elements', 'jonas' ) );
+define( 'VC_CATEGORY', __( 'JONAS Elements', 'theme' ) );
 
 /*
  * TWC Instagram Feed
  */
 
 vc_map( array(
-	"name"     => __( "Instagram Feed", "jonas" ),
+	"name"     => __( "Instagram Feed", "theme" ),
 	"base"     => "twc_insta_feed",
 	"category" => VC_CATEGORY,
 	"icon"     => VC_ICON,
 	"params"   => array(
 		array(
-			'type'       => 'textarea_html',
-			'heading'    => __( 'Titel', 'jonas' ),
+			'type'       => 'textarea',
+			'heading'    => __( 'Titel', 'theme' ),
 			'param_name' => 'content',
-			'holder'     => 'div'
+			'holder'     => 'h4'
 		),
 		array(
 			'type'       => 'textfield',
-			'heading'    => __( 'Instagram shortcode', 'jonas' ),
+			'heading'    => __( 'Instagram tag', 'theme' ),
+			'param_name' => 'ig_tag',
+			'holder'     => 'i'
+		),
+		array(
+			'type'       => 'textfield',
+			'heading'    => __( 'Instagram shortcode', 'theme' ),
 			'param_name' => 'ig_shortcode'
 		)
 	)
@@ -28,10 +34,14 @@ vc_map( array(
 
 add_shortcode( "twc_insta_feed", "twc_insta_feed_shortcode" );
 function twc_insta_feed_shortcode( $attr, $content ) {
-	$html = '<div class="insta-feed">';
+	$html = '<div class="insta-feeds">';
 
+	$html = '<div class="insta-feeds-info">';
 	$html .= '<div class="insta-title">' . $content . '</div>';
-	$html .= '<div class="insta-content">' . do_shortcode( '[' . $attr['ig_shortcode'] . ']' ) . '</div>';
+	$html .= '<div class="insta-tag">' . $attr['ig_tag'] . '</div>';
+	$html .= '</div>';
+
+	$html .= '<div class="insta-feeds-wrapper">' . do_shortcode( '[' . $attr['ig_shortcode'] . ']' ) . '</div>';
 
 	$html .= '</div>';
 
