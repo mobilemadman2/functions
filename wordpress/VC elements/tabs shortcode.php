c6012ac53e89471ecc60c19cda1e03105ac3347c
Minh-Phuc Bui <hiphucbui@gmai.com>
2018-11-28 13:46:51 +0700

    Create tabs shortcode.php

---
+++ wordpress/VC elements/tabs shortcode.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Reuse tabs shortcode
+ */
+$tabs = '[vc_tta_tabs active_section="1" el_id="id" el_class="class"][vc_tta_section title="Tab 1" tab_id="1543332536290-4b9f9b3a-1497" el_class="class"][/vc_tta_section][vc_tta_section title="Tab 2" tab_id="1543332536362-b379a24c-0d27"][/vc_tta_section][/vc_tta_tabs]';
