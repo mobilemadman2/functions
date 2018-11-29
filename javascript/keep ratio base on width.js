445601877a99119ab56829814deaf73fff36097d
Minh-Phuc Bui <hiphucbui@gmai.com>
2018-11-29 09:50:33 +0700

    Create keep ratio base on width.js

---
+++ javascript/keep ratio base on width.js
@@ -0,0 +1,16 @@
+/**
+ * Keep ratio of image base on its width
+ * @param $el : element object
+ * @param x : x ratio
+ * @param y : y ratio
+ */
+TWC.keepRatioBaseOnWidth = function ($el, x, y) {
+    if ($el.length) {
+        var w = $el.width(), h = 0;
+        $window.on('load resize', function () {
+            h = y * w / x;
+            $el.addClass("twc-ratio-kept");
+            $el.height(h);
+        });
+    }
+};
