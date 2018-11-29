02720130488745c5d07e8f3ec5615a7d7583006c
phucbm <hiphucbui@gmail.com>
2018-11-29 15:00:44 +0700

    Update get all post of a custom type.php

--- wordpress/get all post of a custom type.php
+++ wordpress/get all post of a custom type.php
@@ -1,5 +1,12 @@
-$query = new WP_Query(array(
-			'post_type' => 'projecten',
-			'post_status' => 'publish',
-			'posts_per_page' => -1
-		));
+<?php
+$query = new WP_Query( array(
+	'post_type'      => 'projecten',
+	'post_status'    => 'publish',
+	'posts_per_page' => - 1
+) );
+
+$posts = get_posts( array(
+	'post_type'      => 'projecten',
+	'post_status'    => 'publish',
+	'posts_per_page' => - 1
+) );
