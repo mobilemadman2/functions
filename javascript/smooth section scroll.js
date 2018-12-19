7e316dbed2d9f793c7b648d8237e366ff1b35195
phucbm <hiphucbui@gmail.com>
2018-12-19 14:51:28 +0700

    Update smooth section scroll.js

--- javascript/smooth section scroll.js
+++ javascript/smooth section scroll.js
@@ -1,22 +1,49 @@
 /**
- * Smooth section scroll
- * Change default instant move to section with animated scroll to section
+ * Smoothly scroll to section (support "https:domain.com/#target", "#target")
+ * @param $el | required
+ * @param offset | 0px by default
+ * @param speed | 700ms by default
+ * @param callbackBeforeAnimate
+ * @param callbackAfterAnimation
  */
-TWC.smoothSectionScroll = function(){
-    // Select all a tag with href begin with "#"
-    $('a[href^="#"]').each(function () {
-        var href = $(this).attr('href');
+Viivue.smoothSectionScroll = function ($el, offset, speed, callbackBeforeAnimate, callbackAfterAnimation) {
+    // Validation
+    offset = typeof offset === "undefined" ? 0 : offset;
+    speed = typeof speed === "undefined" ? 700 : speed;
 
-        // If is valid href
-        if ($(href).length) {
+    // Select all a tag with href has "#"
+    $el.each(function () {
+        var target = $(this).attr("href");
+
+        // Remove #target from url if any
+        var url = window.location.href;
+        if (url.indexOf("#") > -1) {
+            url = url.substring(0, target.indexOf("#"));
+        }
+
+        // If target has current page link, remove url to get valid target
+        target = target.indexOf(url) > -1 ? target.substring(target.indexOf("#"), target.length) : target;
+
+        // Valid target is started with "#" && exist in current page
+        if (target[0] === "#" && $(target).length) {
+            $(this).attr("data-scroll-target", target);
+            $(this).attr("data-scroll-offset", offset);
             $(this).click(function (e) {
                 e.preventDefault();
 
+                if (typeof callbackAfterAnimation !== "undefined") {
+                    callbackBeforeAnimate();
+                }
+
                 // Smooth scroll
                 $('html,body').animate({
-                    scrollTop: $(href).offset().top
-                }, 700);
+                    scrollTop: $(target).offset().top - offset
+                }, speed);
+
+                if (typeof callbackAfterAnimation !== "undefined") {
+                    callbackAfterAnimation();
+                }
             });
         }
-    })
+    });
 };
