c2e183cd876a5af12c7660b5bb0e85d6a841a87a
phucbm <hiphucbui@gmail.com>
2018-12-26 12:02:10 +0700

    Update get excerpt.php

--- wordpress/get excerpt.php
+++ wordpress/get excerpt.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Get excerpt
+ * Get excerpt (note: wp_trim_excerpt only works in wp loop)
  * https://developer.wordpress.org/reference/hooks/excerpt_length
  * https://developer.wordpress.org/reference/functions/wp_trim_excerpt
+ * https://wordpress.stackexchange.com/questions/268162/get-excerpt-from-post-post-content
  */
 
 // custom length
