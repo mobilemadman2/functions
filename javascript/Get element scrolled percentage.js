47b1e75490cba705254a390f38b7eca11e252dd9
phucbm <hiphucbui@gmail.com>
2019-01-07 23:42:42 +0700

    Update Get element scrolled percentage.js

--- javascript/Get element scrolled percentage.js
+++ javascript/Get element scrolled percentage.js
@@ -1,8 +1,6 @@
 /**
- * Get element's scrolled percentage, run this function in your $(window).scroll event
+ * Get element's scrolled percentage
  * https://teamtreehouse.com/community/get-the-current-percentage-of-scroll-in-an-elementnot-the-document
- * @param $el
- * @returns {number}
  */
 TWC.elementScrolledPercentage = function ($el) {
     var viewportHeight = $(window).height(),
