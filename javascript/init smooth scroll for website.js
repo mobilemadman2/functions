8b595bf320d36377a885a92418d6ca2b4e912334
phucbm <hiphucbui@gmail.com>
2019-01-07 23:42:39 +0700

    Create init smooth scroll for website.js

---
+++ javascript/init smooth scroll for website.js
@@ -0,0 +1,9 @@
+/**
+ * Smooth scroll for website
+ * https://github.com/gblazex/smoothscroll-for-websites/wiki
+ */
+SmoothScroll({
+    // Scrolling Core
+    animationTime    : 1000, // [ms]
+    stepSize         : 60, // [px]
+});
