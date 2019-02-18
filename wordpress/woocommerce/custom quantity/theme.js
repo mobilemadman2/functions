16f5cfd40e4387f7a377ab4f44596d6438172aca
phucbm <hiphucbui@gmail.com>
2019-02-18 10:00:25 +0700

    Update theme.js

--- wordpress/woocommerce/custom quantity/theme.js
+++ wordpress/woocommerce/custom quantity/theme.js
@@ -2,11 +2,13 @@
  * Custom quantity control
  */
 TWC.customQuantity = function () {
-    var $btn = $('.custom-quantity a'),
-        $input = $('.custom-quantity input'),
-        newVal;
+    var $btn = $('.custom-quantity a'), newVal, id, $input;
+
     $btn.click(function () {
+        id = $(this).attr('data-id');
+        $input = $('.custom-quantity input#' + id);
         newVal = parseInt($input.val());
+
         // if up
         if ($(this).hasClass('increase')) {
             newVal += 1;
