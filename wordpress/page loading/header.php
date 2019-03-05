a9c2f91f9423e6381e18804082bc808ea637e80c
phucbm <hiphucbui@gmail.com>
2019-03-05 14:21:05 +0700

    Update header.php

--- wordpress/page loading/header.php
+++ wordpress/page loading/header.php
@@ -1,3 +1,14 @@
-<div class="twc-loading-layer">
-	<img src="<?= THEME_URL ?>/assets/images/three-dots.svg" alt="loading-icon">
+<?php
+/**
+ * Place this on top of header.php as it need to be loaded as quick as possible
+ */
+?>
+<div class="vi-loading-layer">
+	<?php
+	echo wp_get_attachment_image( get_field( "logo", "option" ), 'full', false, array(
+		'title' => get_bloginfo( "name" ),
+		'alt'   => get_bloginfo( "name" ) . " - " . get_bloginfo( "description" ),
+		'class' => 'vi-loading__logo'
+	) );
+	?>
 </div>
