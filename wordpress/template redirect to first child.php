3ae59c4b4b095902cb102cd9b6b1aae023df5521
phucbm <hiphucbui@gmail.com>
2019-03-14 15:14:50 +0700

    Update template redirect to first child.php

--- wordpress/template redirect to first child.php
+++ wordpress/template redirect to first child.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Template Name: Điều hướng tới trang con đầu tiên
+Template Name: Go to the first child page
 */
 if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post();
 		$child_pages = get_pages( "child_of=" . $post->ID . "&sort_column=menu_order" );
 		$first_child = $child_pages[0];
-		wp_redirect( get_permalink( $first_child->ID ) );
+		wp_redirect( get_the_permalink( $first_child->ID ) );
 	}
 }
