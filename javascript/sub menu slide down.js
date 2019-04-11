3e816861d11e6282f55518536049bf68e4d2a729
phucbm <hiphucbui@gmail.com>
2019-04-11 11:49:45 +0700

    Update sub menu slide down.js

--- javascript/sub menu slide down.js
+++ javascript/sub menu slide down.js
@@ -1,27 +1,27 @@
 /**
  * Add slide down button for sub menu
+ * There are two types of click event, please read code to make decision
  */
-VI.menuSlideDownButton = function () {
-    var $items = $('#menu ul.menu > li.menu-item-has-children');
+TWC.mobileMenuSlideDown = function (el) {
+    var $items = $(el); // items have sub menu
     $items.each(function () {
-        $(this).append('<span class="vi-menu-slidedown"></span>');
+        $(this).addClass('menu-item-slidedown');
+        $(this).append('<span class="btn-mobile-slidedown"></span>');
     });
 
     // click event
-    /*
-    var $btn = $('.vi-menu-slidedown');
+    var $btn = $('.btn-mobile-slidedown');
     $btn.click(function () {
-        $(this).prev().slideToggle();
+        $(this).prev().slideToggle(); // toggle slide ul.sub-menu tag
         $(this).toggleClass('close');
     });
-    $btn.trigger('click);
-    */
+    $btn.trigger('click');
 
-    // click event (always open one at once)
-    var $btn = $('.vi-menu-slidedown');
+    // click event (only open one at once, should have from 2 sub menus)
+    /*var $btn = $('.btn-mobile-slidedown');
     $btn.click(function () {
         // close the others
-        var $opening = $('.vi-menu-slidedown:not(.close)');
+        var $opening = $('.btn-mobile-slidedown:not(.close)');
         if ($opening.is($(this))) {
             return;
         }
@@ -31,26 +31,29 @@ VI.menuSlideDownButton = function () {
         // open this
         $(this).prev().slideDown();
         $(this).removeClass('close');
-    });
+    });*/
 
     // trigger close all but first one
-    $btn.prev().slideUp();
+    /*$btn.prev().slideUp();
     $btn.addClass('close');
-    $btn.first().trigger('click');
+    $btn.first().trigger('click');*/
 };
 
 /**
  * CSS
 
- .vi-menu-slidedown {
-    position:absolute; right:5px; top:4px; z-index:1; text-align:center;
+ /* MOBILE MENU > Slide down */
+.menu-item-slidedown {position:relative;}
+.btn-mobile-slidedown {
+    position:absolute; right:-12px; top:-4px; z-index:1; text-align:center;
     width:30px; height:30px; line-height:30px; border-radius:50%; cursor:pointer;
     -webkit-transition:all .3s ease;-o-transition:all .3s ease;transition:all .3s ease;
 }
- .vi-menu-slidedown:hover {background:#fff; color:#2e445f;}
- .vi-menu-slidedown:after {
-    content:'\f106'; font-family:'fontawesome'; display:block;
+.btn-mobile-slidedown:hover {background:#261c5c; color:#fff;}
+.btn-mobile-slidedown:after {
+    content:'\f0d7'; font-family: 'Font Awesome 5 Free'; font-weight:600; display:block;
     -webkit-transition:all .3s ease;-o-transition:all .3s ease;transition:all .3s ease;
+    -webkit-transform:rotate(180deg);-ms-transform:rotate(180deg);transform:rotate(180deg);
 }
- .vi-menu-slidedown.active:after {-webkit-transform:rotate(180deg);-ms-transform:rotate(180deg);transform:rotate(180deg);}
+.btn-mobile-slidedown.close:after {transform:none;}
  */
