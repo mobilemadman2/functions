a46aaf194996864d4470377834d7d8f339765255
phucbm <hiphucbui@gmail.com>
2019-06-24 11:03:20 +0700

    Update gravity form field value shortcode.php

--- wordpress/gravity form/gravity form field value shortcode.php
+++ wordpress/gravity form/gravity form field value shortcode.php
@@ -3,5 +3,5 @@
  * Gravity form shortcode with custom value
  * https://docs.gravityforms.com/allow-field-to-be-populated-dynamically/
  */
-$shortcode = '[gravityform id=2 title=false description=false ajax=true field_values="vacature='.$job->title.'"]';
+$shortcode = '[gravityform id=2 title=false description=false ajax=true]';
 echo do_shortcode($shortcode);
