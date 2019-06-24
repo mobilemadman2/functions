60a96287e125817df74b0082f03ec77ffd3a63ee
phucbm <hiphucbui@gmail.com>
2019-06-24 11:03:17 +0700

    Update get image from id.php

--- wordpress/get image from id.php
+++ wordpress/get image from id.php
@@ -1,3 +1,3 @@
 <?php
-$url = wp_get_attachment_image_src( $thumbnail_id, 'full' )[0];
-$img = wp_get_attachment_image( $thumbnail_id, 'full' );
+$imgURL = wp_get_attachment_image_src( $thumbnail_id, 'full' )[0];
+$imgTag = wp_get_attachment_image( $thumbnail_id, 'full' );
