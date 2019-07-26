ee0e454d9e96407523b7a14c0b183950fc9d5f9e
phucbm <hiphucbui@gmail.com>
2019-07-26 16:11:16 +0700

    Update random - js.js

--- javascript/random - js.js
+++ javascript/random - js.js
@@ -1,5 +1,5 @@
 <script>
-//random() return number between 0 and 1
+//random() return number in [from;to)
 //note: 'from' should equals 0 or 1, test before use
 function random_num(from,to){
 	return Math.floor((Math.random() * to) + from);
